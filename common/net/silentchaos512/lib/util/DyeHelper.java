b135867c4fdd189662800ca37fdedfb029a8da5e
SilentChaos512 <sworkman256@gmail.com>
2016-07-19 18:42:59 -0500

    Add more DyeHelper methods.

--- common/net/silentchaos512/lib/util/DyeHelper.java
+++ common/net/silentchaos512/lib/util/DyeHelper.java
@@ -1,7 +1,12 @@
 package net.silentchaos512.lib.util;
 
+import javax.annotation.Nullable;
+
+import net.minecraft.init.Items;
 import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemDye;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class DyeHelper {
 
@@ -15,5 +20,31 @@ public class DyeHelper {
   public static int getColor(EnumDyeColor dye) {
 
     return ItemDye.DYE_COLORS[dye.getDyeDamage()];
+  }
+
+  public static boolean isItemDye(ItemStack stack) {
+
+    return oreDictDyeToVanilla(stack) != null;
+  }
+
+  @Nullable
+  public static ItemStack oreDictDyeToVanilla(ItemStack stack) {
+
+    for (int id : OreDictionary.getOreIDs(stack)) {
+      String name = OreDictionary.getOreName(id);
+      if (name.startsWith("dye")) {
+        name = name.substring(3);
+        if (name.equals("lightgray"))
+          name = "silver";
+
+        for (int i = 0; i < EnumDyeColor.values().length; ++i) {
+          if (EnumDyeColor.values()[i].getUnlocalizedName().toLowerCase().equals(name)) {
+            return new ItemStack(Items.DYE, 1, i);
+          }
+        }
+      }
+    }
+
+    return null;
   }
 }
