f1043716a58fa9a1bdebbd47308274d1297e3467
SilentChaos512 <sworkman256@gmail.com>
2016-11-15 04:54:47 -0600

    Update SRegistry (generics, fix entity renderer registration)
    Also switching to 1.10.2 because my 1.9.4 environment has exploded and I really don't want to bother with it.

--- common/net/silentchaos512/lib/registry/SRegistry.java
+++ common/net/silentchaos512/lib/registry/SRegistry.java
@@ -11,7 +11,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemModelMesher;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.entity.Render;
-import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
@@ -21,23 +20,46 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.client.registry.IRenderFactory;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.ModContainer;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.oredict.RecipeSorter;
 import net.minecraftforge.oredict.RecipeSorter.Category;
-import net.silentchaos512.lib.block.BlockContainerSL;
-import net.silentchaos512.lib.block.BlockSL;
 import net.silentchaos512.lib.item.ItemBlockSL;
-import net.silentchaos512.lib.item.ItemSL;
+import net.silentchaos512.lib.util.LogHelper;
 
 public class SRegistry {
 
+  // I highly recommend NOT touching these...
   protected final Map<String, Block> blocks = Maps.newHashMap();
   protected final Map<String, Item> items = Maps.newHashMap();
   protected final List<IRegistryObject> registryObjects = Lists.newArrayList();
 
+  /**
+   * A reference to the mod's instance object.
+   */
+  protected Object mod;
+  /**
+   * The LogHelper for the mod, if any. May be used to log error messages.
+   */
+  protected LogHelper logHelper;
+  /**
+   * Automatically incrementing ID number for registering entities.
+   */
+  protected int lastEntityId = -1;
+
+  /**
+   * The mod ID for the mod this SRegistry instance belongs to.
+   */
   public final String modId;
+  /**
+   * The resource prefix for the mod. This is set in the constructor based on the modId.
+   */
   public final String resourcePrefix;
 
   public SRegistry(String modId) {
@@ -46,32 +68,48 @@ public class SRegistry {
     this.resourcePrefix = modId.toLowerCase() + ":";
   }
 
-  public Block registerBlock(BlockSL block) {
+  public SRegistry(String modId, LogHelper logHelper) {
+
+    this(modId);
+    this.logHelper = logHelper;
+  }
+
+  /**
+   * The mod object should be set automatically in preInit, but it can be done manually if that fails.
+   */
+  public void setMod(Object mod) {
+
+    this.mod = mod;
+  }
+
+  /**
+   * Register an IRegistryObject Block (BlockSL, etc.) Uses getName for its key.
+   */
+  public <T extends Block & IRegistryObject> T registerBlock(T block) {
 
     return registerBlock(block, block.getName());
   }
 
-  public Block registerBlock(BlockContainerSL block) {
-
-    return registerBlock(block, block.getName());
-  }
-
-  public Block registerBlock(Block block, String key) {
+  /**
+   * Register a Block. Its name (registry key/name) must be provided. Uses a new ItemBlockSL.
+   */
+  public <T extends Block> T registerBlock(T block, String key) {
 
     return registerBlock(block, key, new ItemBlockSL(block));
   }
 
-  public Block registerBlock(BlockSL block, ItemBlock itemBlock) {
+  /**
+   * Register an IRegistryObject Block (BlockSL, etc.) with a custom ItemBlock. Uses getName for its key.
+   */
+  public <T extends Block & IRegistryObject> T registerBlock(T block, ItemBlock itemBlock) {
 
     return registerBlock(block, block.getName(), itemBlock);
   }
 
-  public Block registerBlock(BlockContainerSL block, ItemBlock itemBlock) {
-
-    return registerBlock(block, block.getName(), itemBlock);
-  }
-
-  public Block registerBlock(Block block, String key, ItemBlock itemBlock) {
+  /**
+   * Register a Block. Its name (registry key/name) and ItemBlock must be provided.
+   */
+  public <T extends Block> T registerBlock(T block, String key, ItemBlock itemBlock) {
 
     blocks.put(key, block);
     if (block instanceof IRegistryObject) {
@@ -85,12 +123,18 @@ public class SRegistry {
     return block;
   }
 
-  public Item registerItem(ItemSL item) {
+  /**
+   * Register an IRegistryObject Item (ItemSL, etc.) Uses getName for its key.
+   */
+  public <T extends Item & IRegistryObject> T registerItem(T item) {
 
     return registerItem(item, item.getName());
   }
 
-  public Item registerItem(Item item, String key) {
+  /**
+   * Register an Item. Its name (registry key/name) must be provided.
+   */
+  public <T extends Item> T registerItem(T item, String key) {
 
     items.put(key, item);
     if (item instanceof IRegistryObject) {
@@ -98,14 +142,35 @@ public class SRegistry {
     }
 
     ResourceLocation resource = new ResourceLocation(resourcePrefix + key);
-    // item.setRegistryName(resource);
     GameRegistry.register(item, resource);
     return item;
   }
 
+  /**
+   * Register a TileEntity. "tile." + resourcePrefix is automatically prepended.
+   */
   public void registerTileEntity(Class<? extends TileEntity> tileClass, String key) {
 
     GameRegistry.registerTileEntity(tileClass, "tile." + resourcePrefix + key);
+  }
+
+  public void registerEntity(Class<? extends Entity> entityClass, String key) {
+
+    registerEntity(entityClass, key, ++lastEntityId, mod, 64, 20, true);
+  }
+
+  public void registerEntity(Class<? extends Entity> entityClass, String key, int trackingRange,
+      int updateFrequency, boolean sendsVelocityUpdates) {
+
+    registerEntity(entityClass, key, ++lastEntityId, mod, trackingRange, updateFrequency,
+        sendsVelocityUpdates);
+  }
+
+  public void registerEntity(Class<? extends Entity> entityClass, String key, int id, Object mod,
+      int trackingRange, int updateFrequency, boolean sendsVelocityUpdates) {
+
+    EntityRegistry.registerModEntity(entityClass, key, id, mod, trackingRange, updateFrequency,
+        sendsVelocityUpdates);
   }
 
   @SideOnly(Side.CLIENT)
@@ -115,15 +180,41 @@ public class SRegistry {
     ClientRegistry.bindTileEntitySpecialRenderer(tileClass, renderer);
   }
 
+  /**
+   * @deprecated Use the factory version registerEntityRenderer(Class, IRenderFactory)
+   */
   @SideOnly(Side.CLIENT)
+  @Deprecated
   public void registerEntityRenderer(Class<? extends Entity> entityClass, Render renderer) {
 
     Minecraft.getMinecraft().getRenderManager().entityRenderMap.put(entityClass, renderer);
   }
 
+  @SideOnly(Side.CLIENT)
+  public <T extends Entity> void registerEntityRenderer(Class<T> entityClass,
+      IRenderFactory<T> renderFactory) {
+
+    RenderingRegistry.registerEntityRenderingHandler(entityClass, renderFactory);
+  }
+
+  /**
+   * Used to register a custom recipe handler. Be sure to catch potential exceptions.
+   * 
+   * @param recipeClass
+   *          The recipe class (extending RecipeBase is recommended, but not required).
+   * @param name
+   *          A name for the recipe. resourcePrefix is automatically prepended.
+   * @param category
+   *          Recipe category.
+   * @param dependencies
+   *          Dependencies, such as "after:minecraft:shapeless"
+   * @return The created IRecipe instance (hanging onto this is recommended)
+   * @throws InstantiationException
+   * @throws IllegalAccessException
+   */
   public IRecipe addRecipeHandler(Class<? extends IRecipe> recipeClass, String name,
       Category category, String dependencies)
-          throws InstantiationException, IllegalAccessException {
+      throws InstantiationException, IllegalAccessException {
 
     IRecipe recipe = recipeClass.newInstance();
     GameRegistry.addRecipe(recipe);
@@ -131,40 +222,73 @@ public class SRegistry {
     return recipe;
   }
 
+  /**
+   * Gets a Block from the blocks map. Not recommended, Block instances should be saved in variables for easy access.
+   */
   public Block getBlock(String key) {
 
     return blocks.get(key);
   }
 
+  /**
+   * Gets an Item from the items map. Not recommended, Item instances should be saved in variables for easy access.
+   */
   public Item getItem(String key) {
 
     return items.get(key);
   }
 
+  /**
+   * Call in the "preInit" phase in your common proxy.
+   */
   public void preInit() {
+
+    if (mod == null) {
+      ModContainer container = Loader.instance().getIndexedModList().get(modId);
+      if (container != null)
+        mod = container.getMod();
+      else if (logHelper != null)
+        logHelper.warning("SRegistry for this mod failed to get the mod instance! This could be"
+            + " because the provided mod ID is incorrect.");
+    }
 
     addOreDictEntries();
   }
 
+  /**
+   * Call in the "init" phase in your common proxy.
+   */
   public void init() {
 
     addRecipes();
   }
 
+  /**
+   * Call in the "postInit" phase in your common proxy.
+   */
   public void postInit() {
 
   }
 
+  /**
+   * Call in the "preInit" phase in your client proxy.
+   */
   public void clientPreInit() {
 
     registerModelVariants();
   }
 
+  /**
+   * Call in the "init" phase in your client proxy.
+   */
   public void clientInit() {
 
     registerModels();
   }
 
+  /**
+   * Call in the "postInit" phase in your client proxy.
+   */
   public void clientPostInit() {
 
   }
