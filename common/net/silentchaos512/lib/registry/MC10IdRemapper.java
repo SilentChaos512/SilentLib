ab872d3ed99028ec3911c46308bbe9a5e33c56df
SilentChaos512 <sworkman256@gmail.com>
2017-03-07 19:17:45 -0600

    Remapping helper (fix for blocks not rendering)

---
+++ common/net/silentchaos512/lib/registry/MC10IdRemapper.java
@@ -0,0 +1,10 @@
+package net.silentchaos512.lib.registry;
+
+import net.minecraftforge.fml.common.event.FMLMissingMappingsEvent.MissingMapping;
+
+public class MC10IdRemapper {
+
+  public static void remap(MissingMapping mismap) {
+
+  }
+}
