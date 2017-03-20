ed19a058cb8af4cc8c526a59edf25f54d9032a0e
SilentChaos512 <sworkman256@gmail.com>
2017-03-20 13:04:38 -0500

    Add WorldHelper

---
+++ common/net/silentchaos512/lib/util/WorldHelper.java
@@ -0,0 +1,19 @@
+package net.silentchaos512.lib.util;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class WorldHelper {
+
+  public static boolean mayPlace(World world, Block block, BlockPos pos, boolean ignoreBB,
+      EnumFacing side, @Nullable Entity entity, ItemStack stack) {
+
+    return world.mayPlace(block, pos, ignoreBB, side, entity);
+  }
+}
