22f97497501abbf695b88753d931aaeaa7716d2a
SilentChaos512 <sworkman256@gmail.com>
2017-12-02 19:37:38 -0600

    Add color blending functions

--- common/net/silentchaos512/lib/util/Color.java
+++ common/net/silentchaos512/lib/util/Color.java
@@ -1,5 +1,7 @@
 package net.silentchaos512.lib.util;
 
+import net.minecraft.util.math.MathHelper;
+
 public class Color {
 
   public static final Color BLACK = new Color(0x0);
@@ -36,6 +38,54 @@ public class Color {
     this.green = green;
     this.blue = blue;
     this.alpha = alpha;
+  }
+
+  public Color blendWith(Color other) {
+
+    return blend(this, other);
+  }
+
+  public static Color blend(Color color1, Color color2) {
+
+    return blend(color1, color2, 0.5f);
+  }
+
+  public static Color blend(Color color1, Color color2, float ratio) {
+
+    int i1 = color1.getColor();
+    int i2 = color2.getColor();
+
+    int color = blend(i1, i2, ratio);
+    return new Color(color);
+  }
+
+  public static int blend(int color1, int color2) {
+
+    return blend(color1, color2, 0.5f);
+  }
+
+  public static int blend(int color1, int color2, float ratio) {
+
+    ratio = MathHelper.clamp(ratio, 0f, 1f);
+    float iRatio = 1f - ratio;
+
+    int a1 = (color1 >> 24 & 0xff);
+    int r1 = ((color1 & 0xff0000) >> 16);
+    int g1 = ((color1 & 0xff00) >> 8);
+    int b1 = (color1 & 0xff);
+
+    int a2 = (color2 >> 24 & 0xff);
+    int r2 = ((color2 & 0xff0000) >> 16);
+    int g2 = ((color2 & 0xff00) >> 8);
+    int b2 = (color2 & 0xff);
+
+    int a = (int)((a1 * iRatio) + (a2 * ratio));
+    int r = (int)((r1 * iRatio) + (r2 * ratio));
+    int g = (int)((g1 * iRatio) + (g2 * ratio));
+    int b = (int)((b1 * iRatio) + (b2 * ratio));
+
+    int color = a << 24 | r << 16 | g << 8 | b;
+    return color;
   }
 
   public int getColor() {
