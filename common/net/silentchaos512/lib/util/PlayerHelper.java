4fa727acbaf129f00bf23f9653600a9573f21d6f
SilentChaos512 <sworkman256@gmail.com>
2018-05-05 13:27:56 -0500

    PlayerHelper getFirstValidStack

--- common/net/silentchaos512/lib/util/PlayerHelper.java
+++ common/net/silentchaos512/lib/util/PlayerHelper.java
@@ -2,10 +2,11 @@ package net.silentchaos512.lib.util;
 
 import java.util.List;
 
+import javax.annotation.Nonnull;
+
 import com.google.common.base.Predicate;
 import com.google.common.collect.Lists;
 
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -63,23 +64,27 @@ public class PlayerHelper {
     }
   }
 
+  @Nonnull
   public static ItemStackList getNonEmptyStacks(EntityPlayer player) {
 
     return getNonEmptyStacks(player, true, true, true, s -> true);
   }
 
+  @Nonnull
   public static ItemStackList getNonEmptyStacks(EntityPlayer player,
       Predicate<ItemStack> predicate) {
 
     return getNonEmptyStacks(player, true, true, true, predicate);
   }
 
+  @Nonnull
   public static ItemStackList getNonEmptyStacks(EntityPlayer player, boolean includeMain,
       boolean includeOffHand, boolean includeArmor) {
 
     return getNonEmptyStacks(player, includeMain, includeOffHand, includeArmor, s -> true);
   }
 
+  @Nonnull
   public static ItemStackList getNonEmptyStacks(EntityPlayer player, boolean includeMain,
       boolean includeOffHand, boolean includeArmor, Predicate<ItemStack> predicate) {
 
@@ -101,6 +106,45 @@ public class PlayerHelper {
           list.add(stack);
 
     return list;
+  }
+
+  /**
+   * Gets the first matching valid ItemStack in the players inventory.
+   * 
+   * @param player
+   *          The player
+   * @param includeMain
+   *          Search the players main inventory (hotbar and the 3 rows above that, hotbar is first, I think).
+   * @param includeOffHand
+   *          Check the player's offhand slot as well.
+   * @param includeArmor
+   *          Check the player's armor slots as well.
+   * @param predicate
+   *          The condition to check for on the ItemStack.
+   * @return The first ItemStack that matches the predicate, or ItemStack.EMPTY if there is no match. Search order is
+   *         offHand, armor, main.
+   * @since 2.3.1
+   */
+  @Nonnull
+  public static ItemStack getFirstValidStack(EntityPlayer player, boolean includeMain,
+      boolean includeOffHand, boolean includeArmor, Predicate<ItemStack> predicate) {
+
+    if (includeOffHand)
+      for (ItemStack stack : player.inventory.offHandInventory)
+        if (!stack.isEmpty() && predicate.apply(stack))
+          return stack;
+
+    if (includeArmor)
+      for (ItemStack stack : player.inventory.armorInventory)
+        if (!stack.isEmpty() && predicate.apply(stack))
+          return stack;
+
+    if (includeMain)
+      for (ItemStack stack : player.inventory.mainInventory)
+        if (!stack.isEmpty() && predicate.apply(stack))
+          return stack;
+
+    return StackHelper.empty();
   }
 
   /** @deprecated Renamed to getNonEmptyStacks */
