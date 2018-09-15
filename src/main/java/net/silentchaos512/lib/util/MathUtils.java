77cf9daf79964a23c687a46686963dd1ac760793
SilentChaos512 <sworkman256@gmail.com>
2018-09-15 14:00:19 -0500

    Add MathUtils

---
+++ src/main/java/net/silentchaos512/lib/util/MathUtils.java
@@ -0,0 +1,140 @@
+/*
+ * Silent Lib -- MathUtils
+ * Copyright (C) 2018 SilentChaos512
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 3
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.silentchaos512.lib.util;
+
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.Vec3i;
+
+import java.util.Random;
+
+public final class MathUtils {
+    private MathUtils() {
+        throw new IllegalAccessError("Utility class");
+    }
+
+    public static AxisAlignedBB boundingBoxByPixels(int minX, int minY, int minZ, int maxX, int maxY, int maxZ) {
+        return new AxisAlignedBB(minX / 16f, minY / 16f, minZ / 16f, maxX / 16f, maxY / 16f, maxZ / 16f);
+    }
+
+    /**
+     * Distance between two {@link Vec3i} (such as {@link net.minecraft.util.math.BlockPos}).
+     * Consider using {@link #distanceSq(Vec3i, Vec3i)} when possible.
+     */
+    public static double distance(Vec3i from, Vec3i to) {
+        int dx = to.getX() - from.getX();
+        int dy = to.getY() - from.getY();
+        int dz = to.getZ() - from.getZ();
+        return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    }
+
+    /**
+     * Distance squared between two {@link Vec3i} (such as {@link net.minecraft.util.math.BlockPos}).
+     * Use instead of {@link #distance(Vec3i, Vec3i)} when possible.
+     */
+    public static double distanceSq(Vec3i from, Vec3i to) {
+        int dx = to.getX() - from.getX();
+        int dy = to.getY() - from.getY();
+        int dz = to.getZ() - from.getZ();
+        return dx * dx + dy * dy + dz * dz;
+    }
+
+    /**
+     * Distance between two {@link Vec3i} (such as {@link net.minecraft.util.math.BlockPos}), but
+     * ignores the Y-coordinate. Consider using {@link #distanceHorizontalSq(Vec3i, Vec3i)} when
+     * possible.
+     */
+    public static double distanceHorizontal(Vec3i from, Vec3i to) {
+        int dx = to.getX() - from.getX();
+        int dz = to.getZ() - from.getZ();
+        return Math.sqrt(dx * dx + dz * dz);
+    }
+
+    /**
+     * Distance squared between two {@link Vec3i} (such as {@link net.minecraft.util.math.BlockPos}),
+     * but ignores the Y-coordinate. Use instead of {@link #distanceHorizontal(Vec3i, Vec3i)} when
+     * possible.
+     */
+    public static double distanceHorizontalSq(Vec3i from, Vec3i to) {
+        int dx = to.getX() - from.getX();
+        int dz = to.getZ() - from.getZ();
+        return dx * dx + dz * dz;
+    }
+
+    public static boolean isInRange(double value, double min, double max) {
+        return value <= max && value >= min;
+    }
+
+    public static int min(final int a, final int b) {
+        return a < b ? a : b;
+    }
+
+    public static int min(int a, final int b, final int c) {
+        if (b < a) a = b;
+        if (c < a) a = c;
+        return a;
+    }
+
+    public static int min(int a, final int b, final int c, final int d) {
+        if (b < a) a = b;
+        if (c < a) a = c;
+        if (d < a) a = d;
+        return a;
+    }
+
+    public static int min(int a, final int b, final int c, final int d, int... rest) {
+        int min = min(a, b, c, d);
+        for (int i : rest)
+            if (i < min)
+                min = i;
+        return min;
+    }
+
+    public static int max(final int a, final int b) {
+        return a > b ? a : b;
+    }
+
+    public static int max(int a, final int b, final int c) {
+        if (b > a) a = b;
+        if (c > a) a = c;
+        return a;
+    }
+
+    public static int max(int a, final int b, final int c, final int d) {
+        if (b > a) a = b;
+        if (c > a) a = c;
+        if (d > a) a = d;
+        return a;
+    }
+
+    public static int max(int a, final int b, final int c, final int d, int... rest) {
+        int max = max(a, b, c, d);
+        for (int i : rest)
+            if (i > max)
+                max = i;
+        return max;
+    }
+
+    public static int nextIntInclusive(Random random, int min, int max) {
+        return random.nextInt(max - min + 1) + min;
+    }
+
+    public static boolean tryPercentage(Random random, double percent) {
+        return random.nextDouble() < percent;
+    }
+}
